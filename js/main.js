696d33224561038df8083cda5149b19a7b209126
syamsiil <muhammadalisyamsi@gmail.com>
2023-02-18 18:22:24 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+// menu.onclick = () => {
+//   navbar.classList.toggle("active");
+// };
+
+// window.onscroll = () => {
+//   navbar.classList.remove("active");
+// };
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
